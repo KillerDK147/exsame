2f284a4277adb6f8e22bae59a0a540d47f30d8f5
Mathias Grundvad Laursen <math181r@skivecollege.dk>
2022-05-03 14:00:03 +0200

    f

--- pages/huske.js
+++ pages/huske.js
@@ -1,32 +1,33 @@
-import { Fragment } from "react";
-import { useState, useEffect } from "react";
-import httpService from "../service/httpService";
-import CardBord from "../comp/CardBord";
-import { getCurrentUser } from "../service/authService";
-import { getBruger } from "../service/huskeService";
+// import { Fragment } from "react";
+// import { useState, useEffect } from "react";
+// import httpService from "../service/httpService";
+// import CardBord from "../comp/CardBord";
+// import { getCurrentUser } from "../service/authService";
+// import { getBruger } from "../service/huskeService";
 const Huske = () => {
-  const [Bruger, setBruger] = useState([]);
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        await getBruger(getCurrentUser()._id).then((res) => {
-          console.log(res.data);
-          setBruger(res.data);
-        });
-        console.log(getCurrentUser()._id);
-      } catch (e) {
-        console.log("pizza er godt");
-        console.log(e.response.error);
-      }
-    };
-    fetchData();
-  }, []);
-  console.log(Bruger[0]);
+  //   const [Bruger, setBruger] = useState([]);
+  //   useEffect(() => {
+  //     const fetchData = async () => {
+  //       try {
+  //         await getBruger(getCurrentUser()._id).then((res) => {
+  //           console.log(res.data);
+  //           setBruger(res.data);
+  //         });
+  //         console.log(getCurrentUser()._id);
+  //       } catch (e) {
+  //         console.log("pizza er godt");
+  //         console.log(e.response.error);
+  //       }
+  //     };
+  //     fetchData();
+  //   }, []);
+  //   console.log(Bruger[0]);
   return (
-    <div>
-      <h1>Huske</h1>
-      <CardBord cards={Bruger} />
-    </div>
+    <h1>Huske</h1>
+    //     <div>
+    //       <h1>Huske</h1>
+    //       <CardBord cards={Bruger} />
+    //     </div>
   );
 };
 
